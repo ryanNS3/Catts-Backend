d96d91b1a93e787a5f0ca50a867321718c3efc01
Ryan <131712164+ryanNS3@users.noreply.github.com>
2026-09-21 22:07:48 -0300

    Criando o projeto

---
+++ src/main/java/com/backend/cattsbackend/CattsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.backend.cattsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CattsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CattsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/backend/cattsbackend/CattsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.cattsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CattsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
